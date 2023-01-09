6a5c0ba5ba94902d219899f60c1affadd7cdba2d
Saurav Rathore <saurav_rathore@epam.com>
2023-01-09 17:11:53 +0530

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to Docker World/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Welcome to Docker World/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
